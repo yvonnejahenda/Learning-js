c1bd1326e325a960c6af180751e37781fc969cf8
yvonnejahenda <35865979+yvonnejahenda@users.noreply.github.com>
2018-03-23 13:07:29 +0300

    add file

---
+++ SwitchCase.js
@@ -0,0 +1,13 @@
+var isSunday = false;
+switch(isSunday){
+  case true:
+  console.log(true)
+  break;
+  case false:
+  console.log("today is not sunday")
+  break;
+  default:
+  console.log("error")
+  break;
+
+}
