44b14a42f72be9f20a991f002585f9b7e2ca6b2d
yvonnejahenda <35865979+yvonnejahenda@users.noreply.github.com>
2018-05-19 08:16:54 +0300

    added file

---
+++ FunctionParameters.js
@@ -0,0 +1,18 @@
+function areaOfTriangle(height, base){
+	var areaOfTriangle =0.5*base*height
+
+	
+	return areaOfTriangle;
+}
+var result = areaOfTriangle(37,10);
+var result2 = areaOfTriangle(76,10);
+var result3 = areaOfTriangle(32,5);
+
+print(result)
+print(result2)
+print(result3)
+
+function print(answer){
+	console.log("areaOfTriangle:"+answer)
+
+}
